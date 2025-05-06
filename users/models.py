f5a7065046b2e75cd27e0f7fa341a191043bffef
Roman <romanshtanhret@gmail.com>
2025-05-06 20:43:53 +0300

    made first migration successfuly, added db users_user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=524, unique=True)
+    role = models.CharField(max_length=50, choices=[("admin", "admin"),("user", "user")], default='user')
